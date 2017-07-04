8dd08687988a2ec0ad5314bd99c62d20b185a8c3
Toohashi <contact.with.hashi@gmail.com>
2017-07-04 21:58:50 +1000

    Add files via upload

---
+++ DOCS/js_index.js
@@ -0,0 +1,5 @@
+/*Go to step 2 -- Terms of Service */
+
+function termsOfServ() {
+    document.getElementById("mainArea").innerHTML="<iframe src=\"terms-of-service.html\" class=\"term-frame\"></iframe><a href=\"sign-up.html\" class=\"agree-button\">I agree and continue</a>";
+}
